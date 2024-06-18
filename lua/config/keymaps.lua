affc945688df9b646a3c2da3e6666569135fea02
Mrzrb <zhangruobin@zuoyebang.com>
2024-06-19 00:20:25 +0800

    Add telescope plugin keymap and configuration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,23 +2,33 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local function keymapOptions(desc)
-    return {
-        noremap = true,
-        silent = true,
-        nowait = true,
-        desc = "GPT prompt " .. desc,
-    }
+  return {
+    noremap = true,
+    silent = true,
+    nowait = true,
+    desc = "GPT prompt " .. desc,
+  }
 end
 
 -- Chat commands
-vim.keymap.set({"n", "i"}, "<C-g>c", "<cmd>GpChatNew<cr>", keymapOptions("New Chat"))
-vim.keymap.set({"n", "i"}, "<C-g>a", "<cmd>GpRewrite<cr>", keymapOptions("New Chat"))
+vim.keymap.set({ "n", "i" }, "<C-g>c", "<cmd>GpChatNew<cr>", keymapOptions("New Chat"))
+vim.keymap.set({ "n", "i" }, "<C-g>a", "<cmd>GpRewrite<cr>", keymapOptions("New Chat"))
 vim.keymap.set("v", "<C-g>c", ":<C-u>'<,'>GpChatNew<cr>", keymapOptions("Visual Chat New"))
-vim.keymap.set({"n", "i"}, "<C-g>r", "<cmd>GpRewrite<cr>", keymapOptions("Inline Rewrite"))
+vim.keymap.set({ "n", "i" }, "<C-g>r", "<cmd>GpRewrite<cr>", keymapOptions("Inline Rewrite"))
 vim.keymap.set("v", "<C-g>r", ":<C-u>'<,'>GpRewrite<cr>", keymapOptions("Visual Rewrite"))
 vim.keymap.set("v", "<C-g>a", ":<C-u>'<,'>GpAppend<cr>", keymapOptions("Visual Append (after)"))
 
-vim.api.nvim_set_keymap('n', ',w', ':w<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gg', ':FloatermNew --height=0.95 --width=0.95 lazygit<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap("n", ",w", ":w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>gg",
+  ":FloatermNew --height=0.95 --width=0.95 lazygit<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>pr",
+  ':lua require("telescope.builtin").grep_string({search = "POST(|GET("})<CR>',
+  { noremap = true, silent = true, desc = "gin router" }
+)
