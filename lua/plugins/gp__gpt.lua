c6ec0148937a830cc9541f9d1a4ce48af6ac85c2
Mrzrb <zhangruobin@zuoyebang.com>
2024-06-18 22:46:35 +0800

    Add lazyvim, flash, hurl, transparent, gp, figutive, floaterm plugins

---
+++ lua/plugins/gp__gpt.lua
@@ -0,0 +1,62 @@
+return {
+  "Robitx/gp.nvim",
+  config = function()
+    require("gp").setup({
+      openai_api_key = "sk-MhNQo7OGg8zWllj8lznzfyjG0xgInKb4ctGJlv3r7sBvS8ND",
+      openai_api_endpoint = "https://api.aiproxy.io/v1/chat/completions",
+      agents = {
+        {
+          name = "ChatGPT4",
+          chat = true,
+          command = false,
+          -- string with model name or table with model name and parameters
+          model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = "You are a general AI assistant.\n\n"
+            .. "The user provided the additional info about how they would like you to respond:\n\n"
+            .. "- If you're unsure don't guess and say you don't know instead.\n"
+            .. "- Ask question if you need clarification to provide better answer.\n"
+            .. "- Think deeply and carefully from first principles step by step.\n"
+            .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+            .. "- Use Socratic method to improve your thinking and coding skills.\n"
+            .. "- Don't elide any code from your output if the answer requires coding.\n"
+            .. "- Take a deep breath; You've got this!\n",
+        },
+        {
+          name = "ChatGPT3-5",
+        },
+        {
+          name = "CodeGPT4",
+          chat = false,
+          command = true,
+          -- string with model name or table with model name and parameters
+          model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = "You are an AI working as a code editor.\n\n"
+            .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+            .. "START AND END YOUR ANSWER WITH:\n\n```",
+        },
+        {
+          name = "CodeGPT3-5",
+          chat = false,
+          command = true,
+          -- string with model name or table with model name and parameters
+          model = { model = "gpt-3.5-turbo-1106", temperature = 0.8, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = "You are an AI working as a code editor.\n\n"
+            .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+            .. "START AND END YOUR ANSWER WITH:\n\n```",
+        },
+      },
+      hooks = {
+        UnitTests = function(gp, params)
+          local template = "I have the following code from {{filename}}:\n\n"
+            .. "```{{filetype}}\n{{selection}}\n```\n\n"
+            .. "Please respond by writing table driven unit tests for the code above."
+          local agent = gp.get_command_agent()
+          gp.Prompt(params, gp.Target.enew, nil, agent.model, template, agent.system_prompt)
+        end,
+      },
+    })
+  end,
+}
