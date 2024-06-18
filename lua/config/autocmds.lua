20bef30c28b2033b3baad8ce50e4e9110077c947
Mrzrb <zhangruobin@zuoyebang.com>
2024-06-19 00:11:56 +0800

    Update lazy-lock.json and lazyvim.json

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,4 +1,3 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add this to your Neovim configuration file (init.lua or equivalent)
--- Ensure you have the 'nvim-lspconfig' and 'nvim-lsp-installer' plugins installed
