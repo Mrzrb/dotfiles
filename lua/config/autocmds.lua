b2120cb71fa44e10b0d41155c7d0f58a32aa0e82
Mrzrb <zhangruobin@zuoyebang.com>
2024-06-18 23:16:53 +0800

    Auto-generated commit message

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,9 +2,3 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add this to your Neovim configuration file (init.lua or equivalent)
 -- Ensure you have the 'nvim-lspconfig' and 'nvim-lsp-installer' plugins installed
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "go" },
-  callback = function()
-    vim.b.autoformat = true
-  end,
-})
